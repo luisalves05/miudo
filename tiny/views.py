9b2a7b678f78ae06e55fff54d0afd82432f750e2
Luis Alves <luisalves05@gmail.com>
2017-07-20 13:55:29 -0300

    Registring urls now

--- tiny/views.py
+++ tiny/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+import uuid
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from .models import Url
 
 def init(request):
     return render(request, "tiny/index.html", {})
+
+def make_url(request):
+    if request.method == "POST":
+        url_site = request.POST['url']
+        url_uuid = str(uuid.uuid4())[0:8]
+        try:
+            url = Url.objects.get(url_uuid = url_uuid)
+        except Url.DoesNotExist:
+            url = Url.objects.create(url_uuid = url_uuid, url_site = url_site)
+            url.save()
+    return HttpResponseRedirect("/")
