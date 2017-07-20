2ce5e7a06912f39baf9eb46cfea188fbe020eeac
Luis Alves <luisalves05@gmail.com>
2017-07-20 09:46:10 -0300

    Creating the models for URL and adding an app

--- tiny/models.py
+++ tiny/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.core.validators import URLValidator
 
 # Create your models here.
+class Url(models.Model):
+    url_id = models.AutoField(primary_key = True)
+    url_site = models.TextField(validators = [URLValidator()])
