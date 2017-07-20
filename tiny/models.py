664dc398424bd888f338ce233958532acbc0ba73
Luis Alves <luisalves05@gmail.com>
2017-07-20 10:46:03 -0300

    Creating first template, and adding url

--- tiny/models.py
+++ tiny/models.py
@@ -3,5 +3,6 @@ from django.core.validators import URLValidator
 
 # Create your models here.
 class Url(models.Model):
-    url_id = models.AutoField(primary_key = True)
+    url_uuid = models.CharField(primary_key = True, max_length=8)
     url_site = models.TextField(validators = [URLValidator()])
+    
--- tiny/views.py
+++ tiny/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def init(request):
+    return render(request, "tiny/index.html", {})
