6d9141f072770d81069d05361cd921137deccf5e
Luis Alves <luisalves05@gmail.com>
2017-07-24 09:07:53 -0300

    Creating a new app for user account

--- shortener-url/urls.py
+++ shortener-url/urls.py
@@ -20,4 +20,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('shortener.urls', namespace = "shortener", app_name = "shortener")),
+    url(r'^account/', include('account.urls', namespace = "account", app_name = "account")),
 ]
