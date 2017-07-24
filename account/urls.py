a58a2f95c624621416efb04973e445c0126f1f8c
Luis Alves <luisalves05@gmail.com>
2017-07-24 10:02:17 -0300

    Using the Django framework for user auth

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    url(r'^login/$', views.user_login, name='login'),
+    #url(r'^login/$', views.user_login, name='login'),
+    url(r'^login/$', auth_views.login, name="login")
 ]
